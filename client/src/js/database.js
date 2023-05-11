8fcd49fbe1a801580af5725791a55ed527b4b5fe
daveS <davesimpcontrol@gmail.com>
2023-05-11 00:19:03 -0600

    first todo addressed properly

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,21 +12,21 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// TODO: T1
+export const putDb = async (content) => { console.error('putDb not implemented');
 console.log('Post to the database');
 
 // Create a connection to the database database and version we want to use.
-const contentDb = await openDB('content', 1);
+const jateDb = await openDB('jate', 1);
 
 // Create a new transaction and specify the database and data privileges.
-const tx = contentDb.transaction('content', 'readwrite');
+const tx = jateDb.transaction('jate', 'readwrite');
 
 // Open up the desired object store.
-const store = tx.objectStore('content');
+const store = tx.objectStore('jate');
 
 // Use the .add() method on the store and pass in the content.
-const request = store.add({ content: content});
+const request = store.add({ jate: content});
 
 // Get confirmation of the request.
 const result = await request;
