8ae11ed08c9f47f7b7ba55bd659596573f13b950
daveS <davesimpcontrol@gmail.com>
2023-05-13 08:42:35 -0600

    added notes for later

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,6 +4,8 @@ const butInstall = document.getElementById('buttonInstall');
 // Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
 
+  // google beforeinstallprompt
+
     // Store the triggered events
     window.deferredPrompt = event;
 
@@ -27,10 +29,14 @@ butInstall.addEventListener('click', async () => {
     window.deferredPrompt = null;
     
     butInstall.classList.toggle('hidden', true);
+    // if app added to windows === app installed
+    // if app installed PREVIOUSLY === true then "app already installed!"
   });
 
 // TODO: T1
 window.addEventListener('appinstalled', (event) => {
   // Clear prompt
+
+  // is this doing anything?
   window.deferredPrompt = null;
 }); 
